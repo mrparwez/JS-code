99a0697e2c29ae3a1b14acbc496192f56a221e0b
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-12 09:20:38 +0000

    some notes on ConversionOpertor

---
+++ JS-Code/04_conversionOperator.js
@@ -0,0 +1,22 @@
+// let score="44abs"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let IsIntValue=Number(score)
+// console.log(IsIntValue)
+// console.group(typeof(IsIntValue))
+
+// let IsTrueOrFlase='myname'
+// console.log(Boolean(IsTrueOrFlase))
+
+
+
+let value=26.7
+// let newval=decim(value)
+console.log(typeof value)
+console.log(String(value))
+/*
+ConversionOperator is way to convert the such type of data store in the variable we can convert it 
+as per our requirmrnts. 
+
+*/ 
