618700a608d01ad329d7b328e362a2665b81dea2
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-12 07:36:24 +0000

    day3_dataType file modified

---
+++ JS-Code/day3_dataType.js
@@ -0,0 +1,38 @@
+"use strict";//treat all JS code as newer version.
+// alert(3+6) We are using nodejs, not browser.
+//Real world cosing mucst have to write the code and code should be readable.
+
+/*
+number =>2 to the power 35
+bigint 
+string =>""
+boolean => True,Flase
+null => standalone value.
+undefined => varibale defined but data not define. 
+symbol => unique..
+
+//Object 
+
+*/
+
+console.log(typeof undefined); // We can see the typeOf data in the variable.
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
