e99d574e7094e4a33e4e573b637b6c2540b148ca
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-12 06:38:20 +0000

    varible file

---
+++ JS-Code/day2_variable.js
@@ -0,0 +1,8 @@
+const AccountId=12345
+let Emp_Name="Parwez Alam"
+var Demmo_Name="Ashraf Ansari" // var keyword is not using now a days to assign value.
+State="Bihar"   //This is also not usable to assign value to  the variable.
+
+// AccountId=123 We can not change the const varible once it Assign
+
+console.table([AccountId,Emp_Name,Demmo_Name,State])
