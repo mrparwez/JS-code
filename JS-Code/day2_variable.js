51dc533899125c92cdeffcfab800b79be3ad1bbe
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-12 06:41:02 +0000

    Add one more file day3 and some change in day2

--- JS-Code/day2_variable.js
+++ JS-Code/day2_variable.js
@@ -4,5 +4,9 @@ var Demmo_Name="Ashraf Ansari" // var keyword is not using now a days to assign 
 State="Bihar"   //This is also not usable to assign value to  the variable.
 
 // AccountId=123 We can not change the const varible once it Assign
+/*
+Preferd not to use var 
+becouse of the issue in Block Scope and Function Scope.
+*/
 
 console.table([AccountId,Emp_Name,Demmo_Name,State])
