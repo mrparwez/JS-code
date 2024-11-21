cfa7cba359ad518346ae5ebc25bdefc6451656a4
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-21 06:21:33 +0000

    Changed folder name JS-Code to Basic_JS

---
+++ Basic_JS/08_date_time.js
@@ -0,0 +1,21 @@
+console.log(new Date().toDateString())
+// console.log(Now.Date())
+const date=new Date()
+// console.log(date.toDateString())
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// date.toISOString()==date.toJSON()
+// console.log(date.toISOString())
+// console.log(date.toJSON())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toTimeString())
+// console.log(date.toUTCString())
+// console.log(date.getDate())
+console.log(date.getDay())
+console.log(date.getDate())
+
+
+
+
+
+
--- JS-Code/08_date_time.js
+++
@@ -1,4 +0,0 @@
-console.log(new Date().toDateString())
-// console.log(Now.Date())
-const date=new Date
-console.log(date.toDateString())
