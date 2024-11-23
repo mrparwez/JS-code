ae7762e567ba813d71a1e94493a3acb929ec967e
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-11-23 16:58:15 +0000

    Arrow completed

--- Advance_JS/Arrow.js
+++ Advance_JS/Arrow.js
@@ -4,12 +4,80 @@ const Prices={
     WelcomeMessage: function(){
         // console.log(`Welcome ${Prices.username} to your site. `)
         // console.log(`${this.username} Welcome to Website !. `)
-        console.log(this);
+        // console.log(this);
         
     }
 }
 
 // Prices.WelcomeMessage()
 Prices.username='Asharf03'
-Prices.WelcomeMessage()
+Prices.WelcomeMessage();
 // console.log(Prices.WelcomeMessage())
+
+// function code(){
+//     console.log(this)
+// }
+// code()
+
+// const code =function(){             // Created function with const function name.
+//     console.log('Hello World')
+// }
+
+// code()
+
+// Arrow Fucntion:
+// const Arr_Func = () =>{
+//     console.log('Hii Arrow function. ')
+// }
+
+// Arr_Func()
+
+// const addTwo = (a,b)=>{
+//     console.log(a+b)   // console and return both workign.
+//     return a+b;
+// }
+// console.log(addTwo(2,5))
+
+
+// function name(){
+
+// }
+
+// (()=>
+//     console.log('Hii')  //Function with no name 
+// )();   // Function Calling
+
+// (function name(){
+//     console.log('I am Normal Function !')  // Function with name of function.
+// })()  // Function Calling.
+
+// (function name(a,b){
+//     console.log(a+b)
+// })(3,4)
+
+
+// function name(a,b){
+//     console.log('Executed')
+// }
+
+// name()
+
+// (function (a,b){
+//     console.log(a+b)
+// })(10,20)
+
+
+// let val1=90
+// let val2=89
+// function AddNum(num1,num2){
+//     total=num1+num2
+//     return total
+// }
+
+// let result1=AddNum(val1,val2)
+// console.log(result1)
+// let result2=AddNum(20,50)
+// console.log(result2)
+
+
+
