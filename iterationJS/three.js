3c228caf360680cf89a25dd6bf3657722aaad6fd
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-12-07 11:47:55 +0000

    added file and filder

--- iterationJS/three.js
+++ iterationJS/three.js
@@ -29,3 +29,11 @@ newArr=[]
 // }
 
 
+const map1= new Map()
+map1.set('IN','India')
+map1.set('USA','United States of America')
+map1.set('FR','France')
+map1.set('IN','India')      //So basically Key sould be unique and value can be duplicate.
+map1.set('IND','India')
+
+console.log(map1);
