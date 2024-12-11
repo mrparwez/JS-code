3389f7d9d21ab3fc1d87790ae3adc296face4b04
Parwez Alam <116522585+mrparwez@users.noreply.github.com>
2024-12-11 10:31:16 +0000

    loops practice

---
+++ iterationJS/five.js
@@ -0,0 +1,93 @@
+// // const coding=['JS','Java','C++','Python']
+// // coding.forEach(function (i){
+// //     console.log(i)
+// // }
+// // )
+
+// // const addTwo = (a,b)=>{
+// //     console.log(a+b)   // console and return both workign.
+// //     return a+b;
+
+// // Arrow function.
+
+// const coding=['JS','Java','C++','Python']
+// coding.forEach(element => {
+//     // console.log(element)
+// });
+
+// for (const i of coding) {
+//     // console.log(i)
+// }
+// for (let i = 0; i < coding.length; i++) {
+//     console.log(coding[i])
+    
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// const Elements=[
+//     {
+//         languageName:'Python',
+//         languageGrade:1
+//     },
+//     {
+//         languageName:'Java',
+//         languageGrade:2
+//     },
+//     {
+//         languageName:'JavaScript',
+//         languageGrade:4
+//     },
+//     {
+//         languageName:'C++',
+//         languageGrade:3
+//     }
+//  ]
+
+
+// for (let index = 0; index < Elements.length; index++) {
+//     const element = Elements[index];
+//     console.log(element.languageName)
+//     console.log(element.languageGrade)
+    
+// }
+
+// const myArray=[1,1,2,1,2,4,4,5,3,2,4,5,6,6,7]
+// console.log(myArray.filter((num)=>num>4))
+// const NewArray=myArray.filter((num)=>num>4)
+// console.log(NewArray)
+
+// const NewArray_1=myArray.filter( (num) =>{
+//     return num >4           //Must have to write return function over here.
+
+// })
+// console.log(NewArray_1)
+
+
+// newNums=[]
+
+// myArray.forEach( (num) =>{
+//     if( num > 4){
+//         newNums.push(num)
+//     } 
+// })
+
+// console.log(newNums)
+
+const myArray=[1,1,2,1,2,4,4,5,3,2,4,5,6,6,7]
+
+// const NewArray=[]
+// myArray.forEach( (num) => {
+//    return NewArray.push(num+10)
+// })
+// console.log(NewArray)
+
+// const result =myArray.forEach( (num) =>(NewArray.push(num+10))) // Autom return.
+// console.log(NewArray);
+
+// const Result=myArray.map( (i) => i+10)
+// console.log(Result)     //There are many way to code in JavaScript........
+
